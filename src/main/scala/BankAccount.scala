ee62a2b0f277027cf2d5628e01e7924b8bf71586
John Park <chongsuh.park@oracle.com>
2017-08-10 15:47:19 -0700

    Update One

--- src/main/scala/BankAccount.scala
+++ src/main/scala/BankAccount.scala
@@ -1,19 +1,74 @@
+import java.util.{Calendar, Date}
+import java.text.SimpleDateFormat
+
 import akka.actor.Actor
 
 /**
   * Created by bclough on 8/10/17.
   */
+
+// Message case classes
+case class Deposit (amount: Double)
+case class Withdraw (amount: Double)
+case class BalanceInquiry ()
+case class ListTransactions()
+
 class BankAccount extends Actor {
 
   var balance = 0.0
-  var transactions:List[Transaction] = Nil
+  var transactions = List[Transaction]()
 
-  class Transaction(amount: Double, date: BigInt)
+  class Transaction(var amount: Double, var date: Date)
+
+  def deposit (amount: Double) = {
+    balance += amount
+    val transaction = new Transaction(amount, Calendar.getInstance().getTime())
+    transactions = transaction::transactions
+  }
+
+  def withdraw (amount: Double) = {
+    balance -= amount
+    val transaction = new Transaction(-amount, Calendar.getInstance().getTime())
+    transactions = transaction::transactions
+  }
+
+  def listTransactions:String = {
+    var returnString = "      Date           Type       Amount\n"
+    returnString +=    "----------------    ------    ----------\n"
+    val dateFormat = new SimpleDateFormat("hh:mm MM-dd-yyyy")
+
+    for (transaction <- transactions){
+      val date = dateFormat.format(transaction.date)
+      val tType = if(transaction.amount > 0) "D" else "W"
+      returnString += s"$date      $tType      " + transaction.amount +"\n"
+
+    }
+
+    returnString
+  }
 
   override def receive: Receive = {
-    case "Deposit" => println("deposited")
-    case "Withdrawal" => println("withdrawn")
-    case none => println("unknown service request")
+
+    case Deposit(amount) => {
+      deposit(amount)
+      println(s"deposited $amount")
+    }
+
+    case Withdraw(amount) => {
+      withdraw(amount)
+      println(s"withdrawn $amount")
+    }
+
+    case BalanceInquiry =>{
+      println(s"Balance: $balance")
+    }
+
+    case ListTransactions =>{
+     println(listTransactions)
+    }
+
+    case _ => println("unknown service request")
+
   }
 
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,15 +3,28 @@
   */
 
 import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 object Main extends App {
 
   // Create the AKKA actor system
   val system = ActorSystem("BankingSystem")
   val accountActor = system.actorOf(Props[BankAccount], name = "accountActor")
+  val timeout = Duration.create(5, "seconds")
 
-  accountActor ! "Deposit"
-  accountActor ! "Withdrawal"
+  var future = accountActor.ask(Deposit(5000))(timeout)
+  var res = Await.result(future, timeout).asInstanceOf[String]
+
+  future = accountActor.ask(Withdraw(5000))(timeout)
+  res = Await.result(future, timeout).asInstanceOf[String]
+
+  future = accountActor.ask(BalanceInquiry)(timeout)
+  res = Await.result(future, timeout).asInstanceOf[String]
+
+  //future = accountActor ? ListTransactions (timeout)
 }
 
