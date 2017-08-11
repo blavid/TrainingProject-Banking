f392393a5381e4d70f151824f3e0e8c1c6875fde
Blake Clough <blakeclough@outlook.com>
2017-08-11 08:52:47 -0700

    Removed extra lines that were not really doing anything.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,9 +4,6 @@
 
 import akka.actor._
 import akka.pattern.ask
-import akka.util.Timeout
-
-import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 object Main extends App {
@@ -14,20 +11,13 @@ object Main extends App {
   // Create the AKKA actor system
   val system = ActorSystem("BankingSystem")
   val accountActor = system.actorOf(Props[BankAccount], name = "accountActor")
-  val timeout = Duration.create(5, "seconds")
+  val timeout = Duration.create(1, "seconds")
 
-  var future = accountActor.ask(Deposit(5000))(timeout)
-  var res = Await.result(future, timeout)
-
-  future = accountActor.ask(Withdraw(3000))(timeout)
-  res = Await.result(future, timeout)
-
-  future = accountActor.ask(Withdraw(300))(timeout)
-  res = Await.result(future, timeout)
-
-  future = accountActor.ask(BalanceInquiry)(timeout)
- res = Await.result(future, timeout)
-
-  future = accountActor.ask(ListTransactions) (timeout)
+  // Create some transactions
+  accountActor.ask(Deposit(5000))(timeout)
+  accountActor.ask(Withdraw(3000))(timeout)
+  accountActor.ask(Withdraw(300))(timeout)
+  accountActor.ask(BalanceInquiry)(timeout)
+  accountActor.ask(ListTransactions) (timeout)
 }
 
